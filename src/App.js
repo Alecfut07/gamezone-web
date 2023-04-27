7cbac5c6842e4b1766d26bed87b52c6d0d58ed3d
Alec Ortega Cisneros <alec.ortega.cisneros@gmail.com>
2023-04-27 10:10:15 -0600

    Sending all 30 products to a table

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [products, setProducts] = useState();
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("https://localhost:7269/products");
+      if (response.ok) {
+        const results = await response.json();
+        setProducts(results);
+      } else {
+        setProducts(null);
+      }
+    })();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <h1>GameZone</h1>
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Name</th>
+            <th scope="col">Price</th>
+            <th scope="col">State</th>
+            <th scope="col">Type</th>
+          </tr>
+        </thead>
+        <tbody>
+          {(products ?? []).map((product) => {
+            return (
+              <tr>
+                <th scope="row">{product.id}</th>
+                <td>{product.name}</td>
+                <td>{product.price}</td>
+                <td>{product.condition.state}</td>
+                <td>{product.edition.type}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </React.Fragment>
   );
 }
 
