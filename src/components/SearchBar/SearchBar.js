7b01608492cec1dde09f0185c00eb15394ad9ee1
Alec Ortega Cisneros <alec.ortega.cisneros@gmail.com>
2023-06-07 20:35:00 -0600

    Change width size of search bar when window size is smaller

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { Button, Form } from "react-bootstrap";
 import { createSearchParams, useNavigate } from "react-router-dom";
 
@@ -10,6 +10,16 @@ function SearchBar() {
   const navigateSearchProductsPage = useNavigate();
 
   const formRef = useRef(null);
+
+  const [matchWindow, setMatchWindow] = useState(
+    window.matchMedia("(min-width: 768px)").matches
+  );
+  // 767 px width
+
+  const searchbar = (isMatch) => ({
+    width: isMatch ? "600px" : "400px",
+    height: "40px",
+  });
 
   const handleSubmit = (event) => {
     const form = event.currentTarget;
@@ -42,12 +52,19 @@ function SearchBar() {
     }
   };
 
+  useEffect(() => {
+    window
+      .matchMedia("(min-width: 768px)")
+      .addEventListener("change", (e) => setMatchWindow(e.matches));
+  });
+
   return (
-    <Form ref={formRef} className="d-flex" onSubmit={handleSubmit}>
+    <Form ref={formRef} className="d-flex" lg={3} onSubmit={handleSubmit}>
       <Form.Control
         type="search"
         placeholder="Search"
-        className="search-bar me-2"
+        className="me-2"
+        style={searchbar(matchWindow)}
         aria-label="Search"
         onChange={onProductNameSearchBarChange}
         required
