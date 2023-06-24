f0934f8393a16b5886bd5e87b1f7b57a9b860606
Alec Ortega Cisneros <alec.ortega.cisneros@gmail.com>
2023-06-24 16:49:13 -0600

    Remove navbar toggle and collapse from CustomNavbar and adjust elements for mobile devices

---
+++ src/components/CustomNavbar/CustomNavbar.js
@@ -0,0 +1,154 @@
+import React, { useEffect, useState, useContext } from "react";
+import {
+  Button,
+  Container,
+  Nav,
+  NavDropdown,
+  Navbar,
+  Stack,
+  Image,
+} from "react-bootstrap";
+import { NavLink, useNavigate, Link } from "react-router-dom";
+import SearchBar from "../SearchBar/SearchBar";
+import Cart from "../Cart/Cart";
+import UsersService from "../../services/UsersService";
+import AuthService from "../../services/AuthService";
+import CartsService from "../../services/CartsService";
+import { AuthContext, CartContext } from "../../context";
+import Sidebar from "../Sidebar/Sidebar";
+import GameZoneLogo from "../../imgs/logos/gamezone_logo.png";
+
+import "./CustomNavbar.css";
+
+function CustomNavbar() {
+  const [loggedInUser, setLoggedInUser] = useState();
+
+  const navigateSignInPage = useNavigate();
+
+  const { isLoggedIn, setLoggedIn } = useContext(AuthContext);
+  const { setCartTotal } = useContext(CartContext);
+
+  const signOut = async () => {
+    try {
+      await AuthService.signOut();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleSignOut = () => {
+    signOut();
+    localStorage.removeItem("access_token");
+    setLoggedIn(false);
+    navigateSignInPage("/users/sign_in");
+  };
+
+  const getProfile = async () => {
+    const accessToken = localStorage.getItem("access_token");
+    try {
+      if (accessToken !== null) {
+        const user = await UsersService.getProfile(accessToken);
+        setLoggedInUser(user);
+        setLoggedIn(true);
+      }
+    } catch (error) {
+      setLoggedIn(false);
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      // const productQuantity = localStorage.getItem("ProductQuantity");
+      try {
+        const { products } = await CartsService.getCart();
+        const totalQuantity = products.reduce(
+          (accumulator, product) => accumulator + product.quantity,
+          0
+        );
+        setCartTotal(totalQuantity);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, []);
+
+  useEffect(() => {
+    getProfile();
+  }, [isLoggedIn]);
+
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Sidebar />
+        <Navbar.Brand as={NavLink} to="/">
+          <Image
+            className="gamezone-logo"
+            src={`${GameZoneLogo}`}
+            style={{ width: "120px" }}
+          />
+        </Navbar.Brand>
+        <SearchBar />
+        {isLoggedIn ? (
+          <Stack direction="horizontal" gap={3}>
+            <NavDropdown
+              // className="ms-5"
+              title={loggedInUser ? loggedInUser.email : ""}
+              id="user-nav-dropdown"
+            >
+              <NavDropdown.Item
+                as={Link}
+                to="/users/profile" /* href="/users/profile" */
+              >
+                Your profile
+              </NavDropdown.Item>
+              {loggedInUser?.is_admin && (
+                <NavDropdown.Item as={Link} to="/admin/categories">
+                  Categories
+                </NavDropdown.Item>
+              )}
+              {loggedInUser?.is_admin && (
+                <NavDropdown.Item as={Link} to="/admin/products">
+                  Products
+                </NavDropdown.Item>
+              )}
+              {loggedInUser?.is_admin && (
+                <NavDropdown.Item as={Link} to="/admin/conditions">
+                  Conditions
+                </NavDropdown.Item>
+              )}
+              <NavDropdown.Item onClick={() => handleSignOut()}>
+                Sign out
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Stack>
+        ) : (
+          <Stack className="mt-auto" direction="horizontal" gap={3}>
+            <Button
+              // className="ms-5"
+              variant="outline-dark"
+              href="/users/sign_in"
+            >
+              Sign in
+            </Button>
+            <Button
+              // className="ms-auto"
+              variant="outline-dark"
+              href="/users/sign_up"
+            >
+              Sign up
+            </Button>
+          </Stack>
+        )}
+        <Nav style={{ marginRight: "30px" }}>
+          <Nav.Link as={NavLink} to="/cart">
+            <div>
+              <Cart />
+            </div>
+          </Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default CustomNavbar;
